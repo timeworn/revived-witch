424cff7618b7a8840a5e2c9a8cac054815b1cfaf
流萤 <43709531+timeworn@users.noreply.github.com>
2025-02-21 01:08:06 -0800

    site-map fix

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const sitemapConfig = {
+  siteUrl: "https://wiki.timeworn.net",
+  generateRobotsTxt: true,
+};
+
+export default sitemapConfig;
--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import { IConfig } from "next-sitemap";
-import siteData from "./src/data/siteData";
-
-const sitemapConfig: IConfig = {
-  siteUrl: siteData.siteUrl,
-  generateRobotsTxt: true,
-};
-
-export default sitemapConfig;
